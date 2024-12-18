e22e017b1c8dc309e7f9ba5b1d7a36bba22f6200
DasTapan <tapands.das@gmail.com>
2024-12-18 11:38:55 +0530

    Add sidenav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
+import { HomeIcon, MessageCircle, Users } from "lucide-react";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className=" border-red-500 border">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio facere
-      alias soluta error animi mollitia temporibus tempore, laborum magni, at
-      sit harum quisquam! Illum labore sunt quasi optio aperiam. Quas.
-    </div>
+    <main className="h-screen flex">
+      <div className="border-red-500 flex flex-col border-2 w-[60px] h-full">
+        <section className="bg-[#115E56] h-[60%] flex flex-col items-center justify-between py-3 text-white">
+          <div className="">
+            <Image
+              src="logo/wingstore_logo.svg"
+              width={30}
+              height={30}
+              alt="wingstore_logo"
+              className="mb-14 mx-auto"
+            />
+            <div className="flex flex-col gap-y-5 items-center">
+              <div className="group bg-transparent hover:bg-white hover:cursor-pointer transition-all rounded-[8px] p-2">
+                <HomeIcon
+                  size={17}
+                  className="group-hover:text-[#115E56] text-[#CCFBEF]"
+                />
+              </div>
+              <div className="group bg-transparent hover:bg-white hover:cursor-pointer transition-all rounded-[8px] p-2">
+                <MessageCircle
+                  size={17}
+                  className="group-hover:text-[#115E56] text-[#CCFBEF]"
+                />
+              </div>
+              <div className="group bg-transparent hover:bg-white hover:cursor-pointer transition-all rounded-[8px] p-2">
+                <Users
+                  size={17}
+                  className="group-hover:text-[#115E56] text-[#CCFBEF]"
+                />
+              </div>
+            </div>
+          </div>
+          <Image
+            src="icons/gear.svg"
+            width={20}
+            height={20}
+            alt="gear_setting"
+          />
+        </section>
+        <section className="h-[40%]"></section>
+      </div>
+      <div className="border overflow-y-scroll flex-grow border-yellow-400 min-h-full">
+        <div className="h-[9999px]"></div>
+      </div>
+    </main>
   );
 }
